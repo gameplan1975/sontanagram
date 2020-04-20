e1b7dde800adcd6f2ea4f43e7eb5a8046187fff2
gameplan1975 <gameplan1975@gmail.com>
2020-04-20 20:44:06 +0000

    .envをgitにあげない、：Ja関連をいったん外す

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :blogs, through: :favorites
     mount_uploader :user_image, UserImageUploader
     has_secure_password
+    validates :name, presence: true
+    validates :email, presence: true
+    
 end
