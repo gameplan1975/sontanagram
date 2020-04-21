a750ad783b224aaff85b9707ff1d402570c37f7e
gameplan1975 <gameplan1975@gmail.com>
2020-04-21 03:23:36 +0000

    test3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,25 +1,40 @@
+
 Rails.application.configure do
     config.cache_classes = true
-config.eager_load = true
+
+    config.eager_load = true
+
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
+
   config.active_storage.service = :local
+
   config.log_level = :debug
+
   config.log_tags = [ :request_id ]
+
   config.action_mailer.perform_caching = false
+
   config.i18n.fallbacks = true
+
   config.active_support.deprecation = :notify
+
   config.log_formatter = ::Logger::Formatter.new
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'hogehoge-fugafuga.herokuapp.com' }
+
+  config.action_mailer.default_url_options = { host: 'sontanagram.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
@@ -30,5 +45,9 @@ config.eager_load = true
     authentication: :plain,
     enable_starttls_auto: true
   }
-end
+
+
+
+
+
 end
